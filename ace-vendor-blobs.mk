04b2a6dd00f17758640eae6fb731475fb4658de9
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-10-25 15:14:56 -0700

    import vendor/qcom/proprietary/ blobs
    Change-Id: Ia5670900bc885f3b38ad5268623a576f09de08b9

--- ace-vendor-blobs.mk
+++ ace-vendor-blobs.mk
@@ -14,7 +14,8 @@
 
 # Prebuilt libraries which are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
-    vendor/htc/ace/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so
+    vendor/htc/ace/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
+    vendor/htc/ace/proprietary/lib/libqc-opt.so:obj/lib/libqc-opt.so
 
 PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/bin/akmd:system/bin/akmd \
@@ -24,7 +25,22 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/htc/ace/proprietary/bin/snd3254:system/bin/snd3254 \
     vendor/htc/ace/proprietary/etc/AudioBTID.csv:system/etc/AudioBTID.csv \
+    vendor/htc/ace/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+    vendor/htc/ace/proprietary/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+    vendor/htc/ace/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+    vendor/htc/ace/proprietary/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
+    vendor/htc/ace/proprietary/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+    vendor/htc/ace/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    vendor/htc/ace/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    vendor/htc/ace/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    vendor/htc/ace/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    vendor/htc/ace/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
+    vendor/htc/ace/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
+    vendor/htc/ace/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
+    vendor/htc/ace/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
+    vendor/htc/ace/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
     vendor/htc/ace/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
+    vendor/htc/ace/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     vendor/htc/ace/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
     vendor/htc/ace/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
     vendor/htc/ace/proprietary/lib/libchromatix_ov8810_default_video.so:system/lib/libchromatix_ov8810_default_video.so \
@@ -41,12 +57,26 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
     vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
     vendor/htc/ace/proprietary/lib/libgemini.so:system/lib/libgemini.so \
+    vendor/htc/ace/proprietary/lib/libgsl.so:system/lib/libgsl.so \
     vendor/htc/ace/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
     vendor/htc/ace/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so
+    vendor/htc/ace/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
     vendor/htc/ace/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
     vendor/htc/ace/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    vendor/htc/ace/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
+    vendor/htc/ace/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+    vendor/htc/ace/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    vendor/htc/ace/proprietary/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
     vendor/htc/ace/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-    vendor/htc/ace/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
     vendor/htc/ace/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
+    vendor/htc/ace/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
+    vendor/htc/ace/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/htc/ace/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+    vendor/htc/ace/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    vendor/htc/ace/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    vendor/htc/ace/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/htc/ace/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/htc/ace/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
+    vendor/htc/ace/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     vendor/htc/ace/proprietary/lib/hw/camera.vendor.msm7x30.so:system/lib/hw/camera.vendor.msm7x30.so \
     vendor/htc/ace/proprietary/lib/hw/gps.spade.so:system/lib/hw/gps.spade.so

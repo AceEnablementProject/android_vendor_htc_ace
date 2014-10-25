d2493671c7a0f809b1d54500b06180d3e8695e57
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-10-25 14:57:11 -0700

    reorganize blobs
    Change-Id: Icecf0d8a65bd188e18a27be30a194b0e183339a6

--- ace-vendor-blobs.mk
+++ ace-vendor-blobs.mk
@@ -14,43 +14,39 @@
 
 # Prebuilt libraries which are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
-    vendor/htc/ace/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so
+    vendor/htc/ace/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so
 
-# misc blobs
 PRODUCT_COPY_FILES += \
-    vendor/htc/ace/proprietary/hw/gps.spade.so:/system/lib/hw/gps.spade.so \
-    vendor/htc/ace/proprietary/akmd:/system/bin/akmd \
-    vendor/htc/ace/proprietary/bma150_usr:/system/bin/bma150_usr \
-    vendor/htc/ace/proprietary/rmt_storage:/system/bin/rmt_storage \
-    vendor/htc/ace/proprietary/snd3254:/system/bin/snd3254 \
-    vendor/htc/ace/proprietary/AudioBTID.csv:/system/etc/AudioBTID.csv \
-    vendor/htc/ace/proprietary/libaudioalsa.so:/system/lib/libaudioalsa.so \
-    vendor/htc/ace/proprietary/libgemini.so:/system/lib/libgemini.so \
-    vendor/htc/ace/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
-    vendor/htc/ace/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so
-
-# camera blobs
-PRODUCT_COPY_FILES += \
-    vendor/htc/ace/proprietary/hw/camera.vendor.msm7x30.so:/system/lib/hw/camera.vendor.msm7x30.so \
-    vendor/htc/ace/proprietary/awb_camera:/system/bin/awb_camera \
-    vendor/htc/ace/proprietary/lsc_camera:/system/bin/lsc_camera \
-    vendor/htc/ace/proprietary/libcameraface.so:/system/lib/libcameraface.so \
-    vendor/htc/ace/proprietary/libcamerapp.so:/system/lib/libcamerapp.so \
-    vendor/htc/ace/proprietary/libchromatix_ov8810_default_video.so:/system/lib/libchromatix_ov8810_default_video.so \
-    vendor/htc/ace/proprietary/libchromatix_ov8810_preview.so:/system/lib/libchromatix_ov8810_preview.so \
-    vendor/htc/ace/proprietary/libchromatix_ov8830_default_video.so:/system/lib/libchromatix_ov8830_default_video.so \
-    vendor/htc/ace/proprietary/libchromatix_ov8830_hfr.so:/system/lib/libchromatix_ov8830_hfr.so \
-    vendor/htc/ace/proprietary/libchromatix_ov8830_preview.so:/system/lib/libchromatix_ov8830_preview.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k3h1gx_default_video.so:/system/lib/libchromatix_s5k3h1gx_default_video.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k3h1gx_preview.so:/system/lib/libchromatix_s5k3h1gx_preview.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k3h2yx_default_video.so:/system/lib/libchromatix_s5k3h2yx_default_video.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k3h2yx_hfr.so:/system/lib/libchromatix_s5k3h2yx_hfr.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k3h2yx_preview.so:/system/lib/libchromatix_s5k3h2yx_preview.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k4e5yx_default_video.so:/system/lib/libchromatix_s5k4e5yx_default_video.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k4e5yx_hfr.so:/system/lib/libchromatix_s5k4e5yx_hfr.so \
-    vendor/htc/ace/proprietary/libchromatix_s5k4e5yx_preview.so:/system/lib/libchromatix_s5k4e5yx_preview.so \
-    vendor/htc/ace/proprietary/libmmipl.so:/system/lib/libmmipl.so \
-    vendor/htc/ace/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so \
-    vendor/htc/ace/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \
-    vendor/htc/ace/proprietary/libOlaEngine.so:/system/lib/libOlaEngine.so \
-    vendor/htc/ace/proprietary/libposteffect.so:/system/lib/libposteffect.so
+    vendor/htc/ace/proprietary/bin/akmd:system/bin/akmd \
+    vendor/htc/ace/proprietary/bin/awb_camera:system/bin/awb_camera \
+    vendor/htc/ace/proprietary/bin/bma150_usr:system/bin/bma150_usr \
+    vendor/htc/ace/proprietary/bin/lsc_camera:system/bin/lsc_camera \
+    vendor/htc/ace/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+    vendor/htc/ace/proprietary/bin/snd3254:system/bin/snd3254 \
+    vendor/htc/ace/proprietary/etc/AudioBTID.csv:system/etc/AudioBTID.csv \
+    vendor/htc/ace/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
+    vendor/htc/ace/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
+    vendor/htc/ace/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8810_default_video.so:system/lib/libchromatix_ov8810_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8810_preview.so:system/lib/libchromatix_ov8810_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8830_default_video.so:system/lib/libchromatix_ov8830_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8830_hfr.so:system/lib/libchromatix_ov8830_hfr.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_ov8830_preview.so:system/lib/libchromatix_ov8830_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h1gx_default_video.so:system/lib/libchromatix_s5k3h1gx_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h1gx_preview.so:system/lib/libchromatix_s5k3h1gx_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h2yx_default_video.so:system/lib/libchromatix_s5k3h2yx_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h2yx_hfr.so:system/lib/libchromatix_s5k3h2yx_hfr.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k3h2yx_preview.so:system/lib/libchromatix_s5k3h2yx_preview.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_default_video.so:system/lib/libchromatix_s5k4e5yx_default_video.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
+    vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
+    vendor/htc/ace/proprietary/lib/libgemini.so:system/lib/libgemini.so \
+    vendor/htc/ace/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
+    vendor/htc/ace/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so
+    vendor/htc/ace/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
+    vendor/htc/ace/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    vendor/htc/ace/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
+    vendor/htc/ace/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
+    vendor/htc/ace/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
+    vendor/htc/ace/proprietary/lib/hw/camera.vendor.msm7x30.so:system/lib/hw/camera.vendor.msm7x30.so \
+    vendor/htc/ace/proprietary/lib/hw/gps.spade.so:system/lib/hw/gps.spade.so

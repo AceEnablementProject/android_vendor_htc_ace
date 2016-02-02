55892b84f04099396afb25472b18eec23831ea1f
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-02-02 13:39:54 +0200

    Update adreno libs KK_2.7_RB1.04.04.00.007.018 from LegacyXperia
    From d2lte.
    Tag AU_LINUX_ANDROID_KK_2.7_RB1.04.04.00.007.018
    
    From:
    OpenDesireProject/android_vendor_qcom_proprietary@c01edd1
    
    Change-Id: I4d9b22a3b24aa1c78a8f1d5964c523bdf59f6e11

--- ace-vendor-blobs.mk
+++ ace-vendor-blobs.mk
@@ -34,13 +34,9 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
     vendor/htc/ace/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     vendor/htc/ace/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/htc/ace/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
     vendor/htc/ace/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
     vendor/htc/ace/proprietary/lib/libOlaEngine.so:system/lib/libOlaEngine.so \
-    vendor/htc/ace/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
-    vendor/htc/ace/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
     vendor/htc/ace/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
-    vendor/htc/ace/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     vendor/htc/ace/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
     vendor/htc/ace/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
     vendor/htc/ace/proprietary/lib/libchromatix_ov8810_default_video.so:system/lib/libchromatix_ov8810_default_video.so \
@@ -57,7 +53,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_hfr.so:system/lib/libchromatix_s5k4e5yx_hfr.so \
     vendor/htc/ace/proprietary/lib/libchromatix_s5k4e5yx_preview.so:system/lib/libchromatix_s5k4e5yx_preview.so \
     vendor/htc/ace/proprietary/lib/libgemini.so:system/lib/libgemini.so \
-    vendor/htc/ace/proprietary/lib/libgsl.so:system/lib/libgsl.so \
     vendor/htc/ace/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
     vendor/htc/ace/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so \
     vendor/htc/ace/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
@@ -70,13 +65,23 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
     vendor/htc/ace/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
     vendor/htc/ace/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
-    vendor/htc/ace/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-    vendor/htc/ace/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-    vendor/htc/ace/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    vendor/htc/ace/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    vendor/htc/ace/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/htc/ace/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/ace/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
-    vendor/htc/ace/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     vendor/htc/ace/proprietary/lib/hw/camera.vendor.msm7x30.so:system/lib/hw/camera.vendor.msm7x30.so \
     vendor/htc/ace/proprietary/lib/hw/gps.spade.so:system/lib/hw/gps.spade.so
+
+# Adrenos
+PRODUCT_COPY_FILES += \
+    vendor/htc/ace/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+    vendor/htc/ace/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
+    vendor/htc/ace/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
+    vendor/htc/ace/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/htc/ace/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/htc/ace/proprietary/vendor/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/egl/libGLESv2S3D_adreno.so \
+    vendor/htc/ace/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
+    vendor/htc/ace/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/htc/ace/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/htc/ace/proprietary/vendor/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
+    vendor/htc/ace/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
+    vendor/htc/ace/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/htc/ace/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/htc/ace/proprietary/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
+    vendor/htc/ace/proprietary/vendor/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so
